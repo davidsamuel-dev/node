8f22ca703ad850f89fe715157e2ecc9806f2d373
davidsamuel-dev <davidsamuel.dev@gmail.com>
2023-10-08 12:38:55 -0300

    cncvbncn

---
+++ calc.js
@@ -0,0 +1,6 @@
+module.exports = function soma(x, y){
+    return x + y;
+}
+module.exports = function mult(x,y){
+    return x * y;
+}
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-var c = process.argv.slice(2)
+let mult = require("./mult")
+let soma = require("./soma")
+
+let args = process.argv.slice(2);
+
+let a = Number(args[1]);
+let b = Number(args[2]);
+let c = "";
+
+if (args[0] == 's')  {
+    c = soma(a,b);
+
+} else if (args[0] == 'm'){;
+    c = mult(a,b);
+
+}else {
+    c = "Opção insválida";
+}
 
 
-if (c == 0){
-    console.log("Informe seu nome:")
-}
-else{
-    console.log('seja bem vindo' + " "  + c)
-}
+
+
+console.log(c);
